e650eafeb6edd6007ed5a64d2adeb0d846cb2b61
AndrewGould <Andrew.Gould@McKesson.com>
2016-08-04 17:36:31 -0400

    small cleanup

--- csharp_exercise/Day7.cs
+++ csharp_exercise/Day7.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.IO;
-using System.Linq;
-
-namespace csharp_exercise
+﻿namespace csharp_exercise
 {
     class Day7
     {
